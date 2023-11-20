2db4ab3f252746f422b7df7d5dcffb0b06d3ca29
AniqKhan03 <aniqahmedkhan123@gmail.com>
2023-11-20 17:45:39 -0500

    removed visionTool() import statement

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
-import {visionTool} from '@sanity/vision'
 
 export default defineConfig({
   name: 'default',
