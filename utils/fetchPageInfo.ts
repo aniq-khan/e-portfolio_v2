0ef3157df08ad144bf6478ad4bdcab31b1e5b7f8
AniqKhan03 <aniqahmedkhan123@gmail.com>
2023-11-20 17:12:56 -0500

    added error handling for fethPageInfo

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,10 +1,16 @@
 import { PageInfo } from "../typings";
 
-export const fetchPageInfo = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
-
-    const data = await res.json()
-    const pageInfo: PageInfo = data.pageInfo;
-
-    return pageInfo;
-}
+export const fetchPageInfo = async () => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
+        if (!res.ok) {
+            throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`);
+        }
+        const data = await res.json();
+        const pageInfo: PageInfo = data.pageInfo;
+        return pageInfo;
+    } catch (error) {
+        console.error("Error fetching page info:", error);
+        throw error; // Re-throw to handle it at a higher level if necessary.
+    }
+};
