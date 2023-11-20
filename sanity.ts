d56ad20a940aaf8d5e14a93193402bd77e98aa64
AniqKhan03 <aniqahmedkhan123@gmail.com>
2023-11-20 17:28:40 -0500

    sanity version change

--- sanity.ts
+++ sanity.ts
@@ -2,13 +2,13 @@ import createImageUrlBuilder from "@sanity/image-url";
 import { createClient } from "next-sanity";
 
 export const config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-    apiVersion: "2021-10-21",
-    useCdn: true,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  apiVersion: "2021-06-07",
+  useCdn: true,
 };
 
 export const sanityClient = createClient(config);
 
-export const urlFor = (source: any) => 
-    createImageUrlBuilder(config).image(source);
+export const urlFor = (source: any) =>
+  createImageUrlBuilder(config).image(source);
